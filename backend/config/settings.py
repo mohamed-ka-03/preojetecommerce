fa91591d2ee295b7e5f3641a0f7da81fa798a558
mohamed-ka-03 <mohamed.ka@edu.ece.fr>
2026-06-25 21:34:04 +0200

    ci: add github actions pipeline

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url;
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -62,16 +63,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
-        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': 'db',
-        'PORT': '5432',
-    }
+    "default": dj_database_url.config(
+        default=f"postgresql://{os.environ.get('POSTGRES_USER', 'postgres')}:{os.environ.get('POSTGRES_PASSWORD', 'postgres')}@db:5432/{os.environ.get('POSTGRES_DB', 'postgres')}"
+    )
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
